0192994119955dc24a1b4fd7dcd8dd253782f98e
Yoruba_chameleon <115434957+BOLUWATIFE-DAVID-ADANLA@users.noreply.github.com>
2025-02-03 19:17:43 +0100

    added apppages js file

---
+++ src/data/app_pages.js
@@ -0,0 +1,8 @@
+
+const AppPages = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  {href: '/quotes', title:"quotes" },
+]
