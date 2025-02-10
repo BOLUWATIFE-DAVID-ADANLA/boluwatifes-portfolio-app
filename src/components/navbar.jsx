45eab1bad1d2adc939997d2bbeb36d844dcbb64e
Yoruba_chameleon <115434957+BOLUWATIFE-DAVID-ADANLA@users.noreply.github.com>
2025-02-10 23:09:52 +0100

    fully optimised for light and dark mode

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,27 +3,26 @@ import AppPages from "../data/app_pages";
 import appText from "../data/appText";
 import HanburgerIcon from "./HanburgerIcon";
 import Closebuttonicon from "./Closebuttonicon";
-import { faMoon } from "@fortawesome/free-regular-svg-icons";
 import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav>
-      <div className=" flex justify-between px-[16px] my-[20px] items-center md:max-w-2xl xl:max-w-5xl mx-auto py-4 sm:border-0 md:border-b-2 md:border-Primary ">
-        <div className="  text-[16px] md:text-2xl font-serif font-semibold">
+      <div className=" flex justify-between px-[16px] my-[20px] items-center md:max-w-2xl xl:max-w-5xl mx-auto py-4 sm:border-0 md:border-b-2 md:border-Primary dark:md:border-DPrimary ">
+        <div className="  text-[16px] text-Text dark:text-DText md:text-2xl font-serif font-semibold">
           {appText.author}
         </div>
 
-        <HanburgerIcon onClick={() => setIsOpen(!isOpen)} />
-
-        {/* this is the desktop view of the nav items  */}
         <div className="flex space-x-6 items-center ">
+          <HanburgerIcon onClick={() => setIsOpen(!isOpen)} />
+
+          {/* this is the desktop view of the nav items  */}
           <div className="  hidden md:flex   space-x-6 ">
             {AppPages.map((link, index) => (
               <a
                 key={index}
                 href={link.path}
-                className="p-2  text-Text font-serif font-medium hover:text-Secondary">
+                className="p-2  text-Text  dark:text-DText font-serif font-medium hover:text-Secondary dark:hover:text-DSecondary">
                 {link.title}
               </a>
             ))}
@@ -38,13 +37,13 @@ const Navbar = () => {
           className="fixed inset-0 bg-transparent bg-opacity-10 z-50 transition-opacity duration-500 ease-in-out"
           onClick={() => setIsOpen(false)}>
           <div
-            className={`w-full h-full bg-Background p-5 absolute right-0 top-0 transform transition-transform duration-700 ease-[cubic-bezier(0.4, 0, 0.2, 1)] ${
+            className={`w-full h-full bg-Background dark:bg-DBackground p-5 absolute right-0 top-0 transform transition-transform duration-700 ease-[cubic-bezier(0.4, 0, 0.2, 1)] ${
               isOpen ? "translate-x-0" : "translate-x-full"
             }`}
             onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
           >
             <button
-              className="text-Text text-2xl absolute top-4 right-4"
+              className="text-Text  dark:text-DText text-2xl absolute top-4 right-4"
               onClick={() => setIsOpen(false)}>
               <Closebuttonicon />
             </button>
@@ -55,7 +54,7 @@ const Navbar = () => {
                 <li key={index}>
                   <a
                     href={link.path} // Regular anchor tag
-                    className="block p-4 text-[22px] font-serif font-semibold text-Text hover:bg-blue-500 hover:text-white rounded transition duration-300 ease-in-out"
+                    className="block p-4 text-[22px] font-serif font-semibold text-Text  dark:text-DText hover:bg-Accent dark:hover:bg-DAccent hover:text-white rounded transition duration-300 ease-in-out"
                     onClick={() => setIsOpen(false)} // Close menu on click
                   >
                     {link.title}
