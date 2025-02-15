a75c10c74c6b54a577c395cfc66c2e09066c7550
Yoruba_chameleon <115434957+BOLUWATIFE-DAVID-ADANLA@users.noreply.github.com>
2025-02-16 00:12:05 +0100

    project page done temporarily

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import SectionContainer from "../components/sectionContainer";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Projects = () => {
   return (
-    <div>projects</div>
-  )
-}
+    <SectionContainer>
+      <div className="space-y-15 px-[20px] md:px-0">
+      <div className="space-y-2">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-Text dark:text-DText">
+          Projects
+        </h1>
+        <h2 className=" text-Text dark:text-DText text-[20px]">
+          Exciting projects are on the way. Stay tuned for updates{" "}
+        </h2>
+      </div>
 
-export default Projects
+      <div className="flex flex-col items-center gap-7">
+        <div className="w-12 h-12 rounded-full bg-Accent dark:bg-DAccent flex items-center justify-center p-2">
+          <FontAwesomeIcon
+            icon={faExclamation}
+            className="text-4xl text-Primary dark:text-DText"
+          />
+        </div>
+        <h2 className="text-center text-lg font-medium max-w-lg dark:text-DPrimary text-Primary">
+        Coming Soon
+        </h2>
+        <h2 className="text-center text-lg font-medium max-w-lg dark:text-DPrimary text-Primary">
+          I am working on some amazing projects. Check back soon for updates on
+          our latest work and innovations.
+        </h2>
+      </div>
+      </div>
+     
+    </SectionContainer>
+  );
+};
+
+export default Projects;
